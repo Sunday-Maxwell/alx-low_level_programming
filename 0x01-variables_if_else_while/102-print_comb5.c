d5cc5d298898c2fc1c46c79a996b7785516ada59
Sunday-Maxwell <chinwendumaxwell54@gmail.com>
2022-09-10 11:43:35 -0700

    Write a program that prints all possible combinations of two two-digit numbers.
    The numbers should range from 0 to 99
    The two numbers should be separated by a space
    All numbers should be printed with two digits. 1 should be printed as 01
    The combination of numbers must be separated by comma, followed by a space
    The combinations of numbers should be printed in ascending order
    00 01 and 01 00 are considered as the same combination of the numbers 0 and 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,27 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
-
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
-
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
+        int lower, num2;
+        for (lower = 0; lower <= 98; lower++) /*print first two digit combo*/
+        {
+                for (num2 = lower + 1; num2 <= 99; num2++)
+                {
+                        putchar((lower / 10) + '0');
+                        putchar((lower % 10) + '0');
+                        putchar(' ');
+                        putchar((num2 / 10) + '0');
+                        putchar((num2 % 10) + '0');
+                        if (lower == 98 && num2 == 99)
+                                continue;
+                        putchar(',');
+                        putchar(' ');
+                }
+        }
+        putchar('\n');
+        return (0);
 }
