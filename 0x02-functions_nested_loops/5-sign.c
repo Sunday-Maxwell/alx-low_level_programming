630996d0e772bcfe7d95f8379432a46541b6bd30
Sunday-Maxwell <chinwendumaxwell54@gmail.com>
2022-09-14 10:48:41 -0700

    print the ssigns of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,13 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('\n');
 	return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar('\n');
 	return (-1);
 	}
 		_putchar('0');
-		_putchar('\n');
 	return (0);
 }
