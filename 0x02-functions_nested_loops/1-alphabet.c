3f2a094a55487c89ddc4de6126199953c73f8862
Sunday-Maxwell <chinwendumaxwell54@gmail.com>
2022-09-14 06:14:41 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alpha
+	{
+		for (alpha = 'a'; <= 'z');
+		alpha++;
+		_putchar(alpha);
+	}
+	return (0);
+}
